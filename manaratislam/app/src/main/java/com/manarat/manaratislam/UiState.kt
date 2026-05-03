3947b55c7d7fda146ac3227ef5c46606ec5d4181
MAHMOUD ABUTEIR <mahmoudteirbusiness@gmail.com>
2026-05-03 13:50:50 +0300

    Delete manaratislam directory

--- manaratislam/app/src/main/java/com/manarat/manaratislam/UiState.kt
+++
@@ -1,9 +0,0 @@
-package com.manarat.manaratislam
-
-sealed interface UiState {
-
-    data object Initial : UiState
-    data object Loading : UiState
-    data class Success(val outputText: String) : UiState
-    data class Error(val errorMessage: String) : UiState
-}
